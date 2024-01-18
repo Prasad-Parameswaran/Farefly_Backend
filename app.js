707ec2cac11bdab6c263f4c9e15f2b8ec255a348
Prasad P <prasadparameswaran1234@gmail.com>
2024-01-18 17:58:23 +0530

    ggggggssssddd

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
+const express = require('express')
+const clientRoute = require('./routes/clientRoute')
+const adminRoute = require('./routes/adminRoute')
+const patner = require('./routes/patnerRoute')
+const app = express()
+const cors = require('cors')
+const http = require('http')
+const mongoose = require('mongoose')
+const { Server } = require("socket.io");
+app.use(cors())
+const server = http.createServer(app)
 
-const app = express();
-const server = http.createServer(app);
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+mongoose.connect(process.env.Database)
+app.use('/', clientRoute)
+app.use("/admin", adminRoute)
+app.use('/partner', patner)
 
-// Enable CORS for the Express server
-app.use(cors());
+//'https://farefly.de-vip.online'
 
-// Create a new Socket.IO server and enable CORS for it
+app.use(cors())
 const io = new Server(server, {
     cors: {
         origin: process.env.baseUrl,
@@ -18,8 +29,33 @@ const io = new Server(server, {
     },
 });
 
-// Your Socket.IO logic goes here
 
-server.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+
+io.on("connection", (socket) => {
+    // Check if the socket is connected
+    if (socket.connected) {
+        console.log("Socket is connected");
+    } else {
+        console.log("Socket is not connected");
+    }
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+
+    socket.on("sentMessage", async () => {
+        console.log(
+            "Connection is on-------------------------------------------------------------"
+        );
+        io.emit("receiveMessage");
+    });
+})
+
+
+
+
+
+
+
+server.listen(4000, () => {
+    console.log('server is running ')
+})
