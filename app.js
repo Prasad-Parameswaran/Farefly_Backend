a10cdfd4039cdf52ef1cae24e56ae8379de66dbb
Prasad P <prasadparameswaran1234@gmail.com>
2024-01-18 17:55:25 +0530

    kk

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
-const express = require('express')
-const clientRoute = require('./routes/clientRoute')
-const adminRoute = require('./routes/adminRoute')
-const patner = require('./routes/patnerRoute')
-const app = express()
-const cors = require('cors')
-const http = require('http')
-const mongoose = require('mongoose')
-const { Server } = require("socket.io");
-app.use(cors())
-const server = http.createServer(app)
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
-require('dotenv').config()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-mongoose.connect(process.env.Database)
-app.use('/', clientRoute)
-app.use("/admin", adminRoute)
-app.use('/partner', patner)
+const app = express();
+const server = http.createServer(app);
 
-//'https://farefly.de-vip.online'
+// Enable CORS for the Express server
+app.use(cors());
 
-app.use(cors())
+// Create a new Socket.IO server and enable CORS for it
 const io = new Server(server, {
     cors: {
         origin: process.env.baseUrl,
@@ -29,33 +18,8 @@ const io = new Server(server, {
     },
 });
 
+// Your Socket.IO logic goes here
 
-
-io.on("connection", (socket) => {
-    // Check if the socket is connected
-    if (socket.connected) {
-        console.log("Socket is connected");
-    } else {
-        console.log("Socket is not connected");
-    }
-    socket.on("disconnect", () => {
-        console.log("User disconnected");
-    });
-
-    socket.on("sentMessage", async () => {
-        console.log(
-            "Connection is on-------------------------------------------------------------"
-        );
-        io.emit("receiveMessage");
-    });
-})
-
-
-
-
-
-
-
-server.listen(4000, () => {
-    console.log('server is running ')
-})
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
